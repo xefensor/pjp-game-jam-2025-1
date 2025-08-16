c2cfe9b5b7a5b641d645ce49c44b7f599a8e4e65
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2025-08-16 18:16:31 +0200

    stuff and things  :]

--- key_button.gd
+++ key_button.gd
@@ -1,11 +1,21 @@
 class_name KeyButton
 extends Button
 
+signal toggled_name(toggled_on: bool, _name: String)
+
 
 func _ready() -> void:
+	add_to_group("Buttons")
+	
 	text = name
 	
 	shortcut = Shortcut.new()
 	var input_event = InputEventKey.new()
 	input_event.physical_keycode = OS.find_keycode_from_string(name)
 	shortcut.events.append(input_event)
+
+	toggled.connect(_on_toggled)
+
+
+func _on_toggled(toggled_on: bool) -> void:
+	toggled_name.emit(toggled_on, name)
--- main.gd
+++ main.gd
@@ -1,2 +1,33 @@
 class_name Main
 extends Control
+
+
+var state_selected: State
+@export var timeline: Array[State]
+var selected_char: String
+
+
+func _ready() -> void:
+	state_selected = timeline[0]
+	connect_buttons()
+
+
+func connect_buttons():
+	for child in get_tree().get_nodes_in_group("Buttons"):
+		if child.has_signal("toggled_name"):
+			child.toggled_name.connect(_on_toggled_name)
+
+
+func _on_toggled_name(toggled_on: bool, _name: String):
+	if _name.is_valid_int():
+		state_selected = timeline[_name.to_int()-1]
+	else:
+		if toggled_on:
+			%KeyAudio.stop()
+			selected_char = _name
+			%KeyAudio.stream = state_selected.sounds[_name]
+			%KeyAudio.play()
+		else:
+			if selected_char == _name:
+				%KeyAudio.stop()
+		
